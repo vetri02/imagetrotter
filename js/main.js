c5ecd3c7afc802aa58e1be72b2cb541b170c7971
Vetrichelvan <vetri02@gmail.com>
2012-10-13 22:02:30 +0530

    basic structure and place autocomplete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+window.imgT = {
+	init: function(){
+		var self = this;
+		
+		self.renderMapLayer();
+		self.placesAutoComplete();
+
+	},
+	renderMapLayer: function(){
+		var layer = new L.StamenTileLayer("watercolor");
+		var map = new L.Map("map", {
+			scrollWheelZoom: false,
+			center: new L.LatLng(37.7, -122.4),
+			minZoom:3,
+		    maxZoom: 8,
+		    zoom: 3
+		});
+		map.addLayer(layer);
+	},
+	placesAutoComplete: function(){
+		$("#geocomplete").geocomplete({
+          details: "form",
+          detailsAttribute: "data-geo"
+        });
+	}
+
+};
+
+$(function(){
+	window.imgT.init();
+})
+
 
